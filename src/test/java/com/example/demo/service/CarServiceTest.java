ec3d813aa7762c937b4c22a63046f3b4378e2334
Anton Rolin <antonrolin@hotmail.com>
2021-05-25 16:08:36 +0200

    added 2 test cases

--- src/test/java/com/example/demo/service/CarServiceTest.java
+++ src/test/java/com/example/demo/service/CarServiceTest.java
@@ -1,19 +1,51 @@
 package com.example.demo.service;
 
 import com.example.demo.models.Car;
+import com.example.demo.repositories.CarRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CarServiceTest {
+    CarService carService;
+
+    @Autowired
+    CarRepository carRepository;
+
+    @BeforeEach
+    public void init(){
+        carService = new CarService(carRepository);
+    }
 
     @Test
     public void getPrice(){
+        int initialPrice = 100000;
+        int productionyear = 2015;
+
         Car car = new Car();
-        car.setProductionyear(2015);
-        car.setInitialPrice(100000);
+        car.setProductionyear(productionyear);
+        car.setInitialPrice(initialPrice);
 
+        int x = carService.getPrice(car, 2021);
 
+        assertEquals(53144, x);
+    }
 
-        assertEquals(53144, 2);
+    @Test
+    @DisplayName("Negative year difference returns initial price")
+    public void getPriceNegativeAge(){
+        int initialPrice = 50000;
+        int productionyear = 2010;
+
+        Car car = new Car();
+        car.setProductionyear(productionyear);
+        car.setInitialPrice(initialPrice);
+
+        int x = carService.getPrice(car, 2008);
+
+        assertEquals(50000, x);
     }
 }
